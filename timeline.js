448f2179845ced380209ae70d8a0be92b5fc4f75
Rozzy <berozzy@gmail.com>
2013-12-31 21:12:25 +0200

    JS Fix

--- timeline.js
+++ timeline.js
@@ -1,1 +1,28 @@
-console.log($.browser)
+$(function() {
+  mobile = $('.mobile').css("display") != "block"
+  alert(mobile);
+  if (!mobile) {
+    debiki.Utterscroll.enable();
+    $("#timeline").mousewheel(function(event, delta) {
+      this.scrollLeft -= (delta * 30);
+      event.preventDefault();
+    });
+  }
+
+  days = [];
+  monthNames = [ "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December" ];
+  function getDaysInMonth(month, year) {
+       // Since no month has fewer than 28 days
+       var date = new Date(year, month, 1);
+       
+       while (date.getMonth() === month) {
+          day = new Date(date);
+          days.push(monthNames[day.getMonth()] + ', ' + day.getDate());
+          date.setDate(date.getDate() + 1);
+       }
+  }
+      
+  for (i = 0; i < 12; i++) getDaysInMonth(i, 2013);
+  console.log(days.length);
+});
