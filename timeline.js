62c8ec8724f8bf0966dbe2aac9a627e43be5ff8e
Rozzy <berozzy@gmail.com>
2013-12-31 20:53:22 +0200

    Autoscroll hack

--- timeline.js
+++ timeline.js
@@ -1,1 +1,29 @@
-console.log($.browser)
+
+
+$(function() {
+
+   $("#timeline").scroll(function(event, delta) {
+    console.log("t");
+      this.scrollRight -= (delta * 30);
+    
+      event.preventDefault();
+
+   });
+
+  days = [];
+  monthNames = [ "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December" ];
+  function getDaysInMonth(month, year) {
+       // Since no month has fewer than 28 days
+       var date = new Date(year, month, 1);
+       
+       while (date.getMonth() === month) {
+          day = new Date(date);
+          days.push(monthNames[day.getMonth()] + ', ' + day.getDate());
+          date.setDate(date.getDate() + 1);
+       }
+  }
+      
+  for (i = 0; i < 12; i++) getDaysInMonth(i, 2013);
+  console.log(days.length);
+});
