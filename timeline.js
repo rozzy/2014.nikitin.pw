956e9a661854f8b502574998a14557dccfc8bc56
Rozzy <berozzy@gmail.com>
2013-12-31 21:30:58 +0200

    timeline days and months

--- timeline.js
+++ timeline.js
@@ -1,14 +1,17 @@
 $(function() {
-  $("#timeline").mousewheel(function(event, delta) {
-    this.scrollLeft -= (delta * 30);
-    event.preventDefault();
-  });
+  mobile = $('.mobile').css("display") != "block"
+
+  if (!mobile) {
+    $("#timeline").mousewheel(function(event, delta) {
+      this.scrollLeft -= (delta * 30);
+      event.preventDefault();
+    });
+  }
 
   days = [];
   monthNames = [ "January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December" ];
   function getDaysInMonth(month, year) {
-       // Since no month has fewer than 28 days
        var date = new Date(year, month, 1);
        
        while (date.getMonth() === month) {
@@ -16,6 +19,7 @@ $(function() {
           days.push(monthNames[day.getMonth()] + ', ' + day.getDate());
           date.setDate(date.getDate() + 1);
        }
+       $("#timeline .days div:eq("+days.length+")").css({height: 20, marginBottom: -5});
   }
       
   for (i = 0; i < 12; i++) getDaysInMonth(i, 2013);
