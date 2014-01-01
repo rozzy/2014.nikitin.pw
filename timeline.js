6eec3942060090b8c6285ea4a023fe0e44dd074d
Rozzy <berozzy@gmail.com>
2014-01-01 19:37:59 +0200

    Sly

--- timeline.js
+++ timeline.js
@@ -1,13 +1,15 @@
 function addGithubContributions (contribs) {
+  contribs = JSON.parse(contribs);
   $.each(contribs, function (key, data) {
     if (data[1] > 0) {
       $('.days div[date="'+data[0]+'"]').append("<span class='github_wrapper'><span class='github_contrubutions'></span></span>");
       for (var i = 0; i < data[1]; i++) {
         $('<span/>', {k: key + i, css: {borderColor: $('.days div[date="'+data[0]+'"]').css('border-color'), opacity: 0.0}, title: data[1]+" github contributions"}).appendTo('.days div[date="'+data[0]+'"] .github_wrapper .github_contrubutions');
-        $('.days div[date="'+data[0]+'"] span[k="'+(key+i)+'"]').animate({opacity: 1.0}, 2500);
-      }
+        $('.days div[date="'+data[0]+'"] span[k="'+(key+i)+'"]').animate({opacity: 1.0}, 2500 + (Math.random() * 2000));
+      }    
     }
   });
+  $("#timeline .days div[title='Today'] .github_contrubutions span").css({marginLeft: 0, 'border-width': 2});
 }
 
 function plusZero (num, month) {
@@ -37,18 +39,15 @@ function getDaysInMonth(month, year) {
     }
 
     $("#timeline .days div:eq("+(days.length + 1)+")").css({height: 20, marginBottom: -5});
+
+    today = new Date();
+    $("#timeline .days div[date='"+(today.getFullYear() + '/' + plusZero(today.getMonth(), true) + '/' + plusZero(today.getDate()))+"']").attr('title', 'Today').css({height: 20, borderWidth: 2, marginBottom: -5});
+
     $('#timeline #dy2013 div:last').css({height: 50, marginBottom: -20});
 }
 
 $(function() {
   mobile = $('.mobile').css("display") != "block"
-
-  if (!mobile) {
-    $("#timeline").mousewheel(function(event, delta) {
-      this.scrollLeft -= (delta * 30);
-      event.preventDefault();
-    });
-  }
 
   $('#timeline').css('overflow', 'hidden');
 
@@ -61,10 +60,25 @@ $(function() {
   monthNames = [ "January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December" ];
   
-  $.get('https://github.com/users/rozzy/contributions_calendar_data?_=1388576157349', addGithubContributions);
+  $.get('contribs.js', addGithubContributions);
 
   $('#timeline #dy2013 div:last').append($("<span/>", {html: '2014', class: 'month_name huge_cap'}));
       
   for (i = 0; i < 12; i++) getDaysInMonth(i, 2013);
   for (i = 0; i < 3; i++) getDaysInMonth(i, 2014);
+
+  $('#timeline').fadeTo(1500, 1.0, function () {
+    var sly = new Sly($('#timeline'), {
+      horizontal: true,
+      itemSelector: '.days',
+      mouseDragging: 1,
+      touchDragging: 1,
+      releaseSwing: 1,
+      keyboardNavBy: 'items',
+      scrollBy: 10,
+      startAt: 0
+    }).init();
+    sly.slideTo(($('.days div[title="Today"]').offset().left - 750), 2300);
+  });
+
 });
