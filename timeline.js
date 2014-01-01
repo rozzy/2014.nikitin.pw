4560f1c53ea1b2f0b8270d392e6fbcbc9bbe5056
Rozzy <berozzy@gmail.com>
2014-01-01 13:35:36 +0200

    Colored timeline with dates

--- timeline.js
+++ timeline.js
@@ -20,25 +20,27 @@ $(function() {
   "July", "August", "September", "October", "November", "December" ];
   
   function getDaysInMonth(month, year) {
-       var date = new Date(year, month, 1);
-       
-       while (date.getMonth() === month) {
-          day = new Date(date);
-          days.push(monthNames[day.getMonth()] + ', ' + day.getDate());
-          $('#timeline #dy'+year+' div:eq('+day.getDOY()+')').attr("title", monthNames[day.getMonth()] + ', ' + day.getDate());
-          date.setDate(date.getDate() + 1); 
-       }
+      var date = new Date(year, month, 1);
+
+      var numberOfItems = 90;
+      var rainbow = new Rainbow(); 
+      rainbow.setNumberRange(1, numberOfItems);
+      rainbow.setSpectrum('#C0D8E8', '#8DA7D0');
+      for (var i = 0; i < numberOfItems; i++) $("#dy2014 div:eq("+i+"), #dy2013 div:eq("+(numberOfItems-i-1)+")").css("border-color", '#'+rainbow.colourAt(i));
+
+      while (date.getMonth() === month) {
+        day = new Date(date);
+        days.push(monthNames[day.getMonth()] + ', ' + day.getDate());
+        $('#timeline #dy'+year+' div:eq('+day.getDOY()+')').attr("title", monthNames[day.getMonth()] + ', ' + day.getDate());
+        if (day.getDate() == 1 && year == 2013) $('#timeline #dy'+year+' div:eq('+day.getDOY()+')').append($("<span/>", {html: monthNames[day.getMonth()].substr(0, 3), css: {color: $('#timeline #dy'+year+' div:eq('+day.getDOY()+')').css('border-color')}}));
+        date.setDate(date.getDate() + 1); 
+      }
 
       $("#timeline .days div:eq("+days.length+")").css({height: 20, marginBottom: -5});
-      $('#timeline #dy2013 div:last').css({height: 50, marginBottom: -25})
-      if (year == 2014) {
-        var numberOfItems = 90;
-        var rainbow = new Rainbow(); 
-        rainbow.setNumberRange(1, numberOfItems);
-        rainbow.setSpectrum('#C0D8E8', '#8DA7D0');
-        for (var i = 0; i < numberOfItems; i++) $("#dy2014 div:eq("+i+")").css("border-color", '#'+rainbow.colourAt(i));
-      }
+      $('#timeline #dy2013 div:last').css({height: 50, marginBottom: -20});
   }
+
+  $('#timeline #dy2013 div:last').append($("<span/>", {html: '2014', class: 'huge_cap'}));
       
   for (i = 0; i < 12; i++) getDaysInMonth(i, 2013);
   for (i = 0; i < 3; i++) getDaysInMonth(i, 2014);
